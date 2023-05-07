28ec6aa78f671ed0371556fbde6994e6de01ec5c
Phat Ha <pattrickha@gmail.com>
2023-05-07 16:05:18 +0700

    Feat: Added sorted features to column names

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,25 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import './Leaderboard.style.css';
 
-const Leaderboard = ({ users }) => {
+const Leaderboard = ({ sortedUsers }) => {
+  const [sortOrderUsers, setSortOrderUsers] = useState(0);
+  const [sortOrderAnswered, setSortOrderAnswered] = useState(0);
+  const [sortOrderCreated, setSortOrderCreated] = useState(0);
+  const [sortedUsersState, setSortedUsersState] = useState(sortedUsers);
+
+  const handleSortedUsers = () => {
+    setSortedUsersState(Object.values(sortedUsers).sort(
+      (first, second) =>
+        sortOrderUsers === 0 ?
+          first.name.localeCompare(second.name)
+          :
+          second.name.localeCompare(first.name)
+    ));
+
+    setSortOrderUsers(sortOrderUsers === 0 ? 1 : 0);
+  };
+
+  const handleSortedUsersAnswered = () => {
+    setSortedUsersState(Object.values(sortedUsers).sort(
+      (first, second) =>
+        sortOrderAnswered === 0 ?
+          Object.keys(first.answers).length - Object.keys(second.answers).length
+          :
+          Object.keys(second.answers).length - Object.keys(first.answers).length
+    ));
+
+    setSortOrderAnswered(sortOrderAnswered === 0 ? 1 : 0);
+  };
+
+  const handleSortedUsersQuestions = () => {
+    setSortedUsersState(Object.values(sortedUsers).sort(
+      (first, second) =>
+        sortOrderCreated === 0 ?
+          first.questions.length - second.questions.length
+          :
+          second.questions.length - first.questions.length
+    ));
+
+    setSortOrderCreated(sortOrderCreated === 0 ? 1 : 0);
+  };
+
   let result = <p>No users found</p>;
 
   result = (
-    <table className="table table-striped mt-2">
+    <table className='table table-striped mt-2'>
       <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Users</th>
-          <th scope="col">Answered</th>
-          <th scope="col">Created</th>
+        <tr className='text-secondary'>
+          <th scope='col'>#</th>
+          <th
+            scope='col'
+            className='tableColumn'
+            onClick={handleSortedUsers}
+          >
+            <span>Users</span>
+            <span className='material-icons-round' id='answeredArrow'>
+              {
+                sortOrderUsers === 0 ? 'arrow_downward' : 'arrow_upward'
+              }
+            </span>
+          </th>
+          <th
+            scope='col'
+            className='tableColumn'
+            onClick={handleSortedUsersAnswered}
+          >
+            <span>Answered</span>
+            <span className='material-icons-round' id='answeredArrow'>
+              {
+                sortOrderAnswered === 0 ? 'arrow_downward' : 'arrow_upward'
+              }
+            </span>
+          </th>
+          <th
+            scope='col'
+            className='tableColumn'
+            onClick={handleSortedUsersQuestions}
+          >
+            <span>Created</span>
+            <span className='material-icons-round' id='createdArrow'>
+              {
+                sortOrderCreated === 0 ? 'arrow_downward' : 'arrow_upward'
+              }
+            </span>
+          </th>
         </tr>
       </thead>
       <tbody>
         {
-          Object.values(users).map(
+          Object.values(sortedUsersState).map(
             (user, index) => (
               <tr key={user.id}>
-                <th scope="row">{index + 1}</th>
+                <th scope='row'>{index + 1}</th>
                 <th scope='row'>{user.name}</th>
                 <th scope='row'>{Object.keys(user.answers).length}</th>
                 <th scope='row'>{user.questions.length}</th>
@@ -43,11 +118,13 @@ const Leaderboard = ({ users }) => {
 
 const mapStateToProps = ({
   users
-}) => {
-  // TODO: Sort users by the sum of the number of questions they’ve answered and the number of questions they’ve asked
-  return {
-    users
-  };
-};
+}) => ({
+  sortedUsers: Object.values(users).sort(
+    (first, second) =>
+      Object.keys(second.answers).length - Object.keys(first.answers).length
+      &&
+      second.questions.length - first.questions.length
+  ),
+});
 
 export default connect(mapStateToProps)(Leaderboard);
