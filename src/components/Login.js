993b84ecc77de5f39126b23f49ad4426690219b9
Phat Ha <pattrickha@gmail.com>
2023-05-11 21:01:47 +0700

    Feat: Added validation using Yup for Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,14 @@ import Input from './common/Input';
 import LoadingBar from 'react-redux-loading-bar';
 import React, { useEffect } from 'react';
 import withRouter from '../utils/routerHelper';
+import * as Yup from 'yup';
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Required'),
+  password: Yup.string()
+    .required('Required'),
+});
 
 const Login = ({ dispatch, authUser, navigate }) => {
   useEffect(() => {
@@ -14,6 +22,16 @@ const Login = ({ dispatch, authUser, navigate }) => {
       navigate('/');
     }
   }, [authUser, navigate]);
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      dispatch(handleAuthUser(values.username, values.password));
+
+      values = { username: '', password: '' };
+
+      setSubmitting(false);
+    }, 400);
+  };
 
   return (
     <>
@@ -32,15 +50,9 @@ const Login = ({ dispatch, authUser, navigate }) => {
           <Formik
             initialValues={{ username: '', password: '' }}
 
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                dispatch(handleAuthUser(values.username, values.password));
+            validationSchema={LoginSchema}
 
-                values = { username: '', password: '' };
-
-                setSubmitting(false);
-              }, 400);
-            }}
+            onSubmit={handleSubmit}
           >
             {
               ({
@@ -49,6 +61,8 @@ const Login = ({ dispatch, authUser, navigate }) => {
                 handleBlur,
                 handleSubmit,
                 isSubmitting,
+                errors,
+                touched,
               }) => (
                 <form onSubmit={handleSubmit}>
                   <div className="mb-3">
@@ -59,8 +73,12 @@ const Login = ({ dispatch, authUser, navigate }) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.username}
-                      isRequired={true}
+                      isRequired={false}
+                      additionalClassNames={errors.username && touched.username ? 'border-danger' : ''}
                     />
+                    {errors.username && touched.username ? (
+                      <div className='text-danger '><small>{errors.username}</small></div>
+                    ) : null}
                   </div>
                   <div className="mb-3">
                     <Input
@@ -70,12 +88,16 @@ const Login = ({ dispatch, authUser, navigate }) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.password}
-                      isRequired={true}
+                      additionalClassNames={errors.password && touched.password ? 'border-danger' : ''}
+                      isRequired={false}
                     />
+                    {errors.password && touched.password ? (
+                      <div className='text-danger'><small>{errors.password}</small></div>
+                    ) : null}
                   </div>
                   <Button
                     type='submit'
-                    isDisabled={isSubmitting}
+                    isDisabled={isSubmitting || Object.keys(errors).length !== 0}
                     name={isSubmitting ? 'Logging in...' : 'Login'}
                   />
                 </form>
